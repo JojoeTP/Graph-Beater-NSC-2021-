0e9824af9763ec4cb44a9eb425b7bc929e22a3ff
Panit <panit.cuteza@mail.kmutt.ac.th>
2022-01-15 13:04:21 +0700

    Cute 15/1/2022
    Update Game3 script

--- Assets/Cute/Script/Game3/Game3Manager.cs
+++ Assets/Cute/Script/Game3/Game3Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Game3Manager : MonoBehaviour
 {
@@ -9,12 +10,24 @@ public class Game3Manager : MonoBehaviour
     public bool isGameStarted = false;
     PlayerController player;
     public Transform endGameWarpPosition;
+    public GameObject tutorialUI;
     public GameObject winCanvas;
+
+    [Header("Camera")]
+    public CinemachineVirtualCamera game3Cam;
+    public List<Transform> levelCam = new List<Transform>();
+    public int level = 1;
+    ButtonWarp buttonWarp;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerController>().GetComponent<PlayerController>();
+        buttonWarp = FindObjectOfType<ButtonWarp>().GetComponent<ButtonWarp>();
+    }
+
+    private void FixedUpdate() {
+        SwitchCam();
     }
 
     public void EndGame(){
@@ -30,7 +43,22 @@ public class Game3Manager : MonoBehaviour
         if(other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E) && !isGameStarted){
             //Play game2
             isGameStarted = true;
-            player.CurrentStage = GameStage.GAME4;
+            player.CurrentStage = GameStage.GAME3;
+            StartGame();
         }
+    }
+
+    public void StartGame(){
+        tutorialUI.SetActive(true);
+
+        //use GenerateQuestion() in button in tutorialUI
+    }
+
+    void SwitchCam(){
+        if(isGameStarted){
+            game3Cam.LookAt = levelCam[level-1];
+            game3Cam.Follow = levelCam[level-1];
+        }
+        
     }
 }
